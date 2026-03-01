147ad8e344ca3d25a4333a92f975469918fc04d1
webiquique <webmasteriquique@gmail.com>
2026-02-28 23:22:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Define aquí los nombres de tus imágenes
+  const images = [
+    'img/milenkaccesoriosmejor.png',
+    'img/Milenkaccesoriouno.png',
+    'img/milenka_aviso.png'
+  ];
+
+  let currentIndex = 0;
+  const slideshowDiv = document.getElementById('slideshow');
+
+  function showImage(index) {
+    slideshowDiv.innerHTML = `<img src="${images[index]}" alt="Publicidad ${index + 1}">`;
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  // Mostrar primera imagen
+  showImage(currentIndex);
+
+  // Cambiar cada 3 segundos
+  setInterval(nextImage, 3000);
+});
